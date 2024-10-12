29d3b1e05b4ba4a2d92e9d48bb370787a083a6e6
ejwebdev <ejwebdev25@gmail.com>
2024-10-12 23:01:29 +0800

    Tailwind and React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./components/header-page/Header.jsx";
 import Default from "./pages/Default.jsx";
 import Products from "./components/products-page/Products.jsx";
@@ -9,6 +10,13 @@ import Banner2 from "./components/banner2-page/Banner2.jsx";
 import Footer from "./components/footer-page/Footer.jsx";
 
 function App() {
+    useEffect(() => {
+        const location = window.location.pathname;
+        window.gtag("config", "G-B0H2ZT4BZ9", {
+            page_path: location,
+        });
+    }, []);
+
     return (
         <HashRouter>
             <Header />
